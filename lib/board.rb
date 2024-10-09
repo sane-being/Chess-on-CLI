cce6dd027aa66a95f3a2784af243f89d4c85aa68
Shreyas <117995509+sane-being@users.noreply.github.com>
2024-10-09 16:23:09 +0530

    add #play, #make_move, #decode_move

--- lib/board.rb
+++ lib/board.rb
@@ -1,40 +1,58 @@
-require_relative 'piece', 'possible_moves'
+require_relative 'piece'
 
 class Board
-  include PossibleMoves
-  attr_accessor :current_state
+  attr_accessor :board_h, :turn_of
 
   def initialize
     # board hash of 64 squares, square => occupying piece ('' if empty)
-    @current_state = create_new_board
+    @board_h = create_new_board
+    @turn_of = :white
+    @moves_a = []
+  end
+
+  def play
+    move = gets
+    decode_move(move) in [square_from, square_to]
+    move = make_move(square_from, square_to)
+    @moves_a.push(move)
+    turn_of = turn_of == :white ? :black : :white
+  end
+
+  def decode_move(move)
+    move = move.split
+    square_from, square_to = move[0..1], move[2..3] # rubocop:disable Style/ParallelAssignment
+    [square_from, square_to].each do |square|
+      square[0] = square[0].ord - 96
+      square[1] = square[1].to_i
+    end
+    [square_from, square_to]
+  end
+
+  def make_move(square_from, square_to)
+    piece = @board_h[square_from]
+    dying_piece = @board_h[square_to]
+    kill = dying_piece && dying_piece.color != turn_of ? true : false
+    [piece, square_from, kill, square_to, check]
   end
 
   private
 
   def create_new_board
-    ## Array of 64 squares: ['a',1] to ['h',8]
-    new_board = ('a'..'h').to_a.product((1..8).to_a)
+    ## Array of 64 squares: [1,1] to [8,8]
+    new_board = (1..8).to_a.product((1..8).to_a)
     # creating empty Board hash,
-    new_board = new_board.to_h { |square| [square, ''] }
+    new_board = new_board.to_h { |square| [square, nil] }
 
     # placing pawns
-    ('a'..'h').each do |col|
+    (1..8).each do |col|
       new_board[[col, 2]] = Piece.new('pawn', :white)
       new_board[[col, 7]] = Piece.new('pawn', :black)
     end
 
     # placing remaining pieces
-    col_of_pieces = {
-      'a' => 'rook',
-      'b' => 'knight',
-      'c' => 'bishop',
-      'd' => 'queen',
-      'e' => 'king',
-      'f' => 'bishop',
-      'g' => 'knight',
-      'h' => 'rook'
-    }
-    col_of_pieces.each do |col, piece|
+    pieces_seq = %w[rook knight bishop queen king bishop knight rook]
+    pieces_seq.each_with_index do |piece, i|
+      col = i + 1
       new_board[[col, 1]] = Piece.new(piece, :white)
       new_board[[col, 8]] = Piece.new(piece, :black)
     end
@@ -43,6 +61,6 @@ class Board
   end
 
   def to_s
-    current_state
+    board_h
   end
 end
